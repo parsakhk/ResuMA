9969aada7bfdfb64ac809ec7b263efaa64bb8b3f
parsakhk <parsakhk88@gmail.com>
2024-01-22 21:17:36 +0330

    Setup of resuma website

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import './Ellipse.css'
+
+function App() {
+
+  return (
+    <>
+      {/** Main page and its ellipses. */}
+      <div className='main-Page'>
+        <div className='ellipse-group'>
+          <div className='ellipse-1'></div>
+          <div className='ellipse-2'></div>
+          <div className='ellipse-3'></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
